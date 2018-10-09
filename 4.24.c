e61162bf2795fb7ae23b36b96920a15e70b41ba9
Khaled Mohamed <khaledm9525@gmail.com>
2018-10-09 23:49:44 +0200

    Add files via upload

---
+++ 4.24.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int i=1;
+int j=2;
+int k=3;
+int m=2;
+
+int main(void)
+{
+
+printf("%d\n",i==1);
+printf("%d\n",j==3);
+
+printf("%d\n",i>=1&&j<4);
+
+printf("%d\n",m<=99&&k<m);
+
+printf("%d\n",j>=i||k==m);
+
+printf("%d\n",k+m<j||3-j>=k);
+
+printf("%d\n",!m);
+
+printf("%d\n",!(j-m));
+
+printf("%d\n",!(k>m));
+
+printf("%d\n",!(j>k));
+
+
+
+return 0;
+}
+
+
+
